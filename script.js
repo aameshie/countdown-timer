2111687b0d74129af5db592d8afb40ca3442b4b8
elameshie <elameshie@mobilus.com>
2024-03-14 12:18:27 -0400

    added some comments to help through the logic.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const currentDate = new Date();
 console.log(currentDate); 
 
+
+
+// The target date for countdown
 const targetDate = new Date('2024-03-14T23:59:59');
 console.log(targetDate);
+
+
+// Subtracts current time from target date to show remainder
+const difference = targetDate - currentDate; 
+console.log(difference);
+
+
