794df47e18e530a05efa984e5245829e07acf811
elameshie <elameshie@mobilus.com>
2024-03-14 14:40:25 -0400

    redid the math for the countdown

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ function updateCountdown() {
   const difference = targetDate - currentTime;
 
   const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(difference % (1000 * 60 * 60 * 24));
-  const minutes = Math.floor(difference % ((difference %  (1000 * 60 * 60)) / (1000 * 60)));
-  const seconds = Math.floor(difference % (difference %  (1000 * 60)) / (1000 * 60));
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 *60));
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 *60));
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
   document.getElementById("days").innerText = days;
   document.getElementById("hours").innerText = hours;
